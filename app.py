f4b63bad489ab0aba23301bde1195baf630505f3
Ben Stone <hello@benstone.tech>
2019-03-14 15:54:42 +0000

    Set env variables better

--- app.py
+++ app.py
@@ -39,8 +39,6 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 
 
-app.config.from_envvar('APPLICATION_SETTINGS')
-
 app.config.from_envvar('DATABASE_URI')
 app.config.from_envvar('SECRET_KEY')
 

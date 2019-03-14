332b7f9c678a07cd6527f7743be851857e014d02
Ben Stone <hello@benstone.tech>
2019-03-14 19:40:04 +0000

    os.environ.get()

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 
-test = app.config.from_object(os.environ['DATABASE_URL'])
+
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 # app.secret_key = os.environ.get('SECRET_KEY')
 
